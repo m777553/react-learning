31b2e1768feadfc9c3d23e83115db842cf77829a
Marina <Marina717@ya.ru>
2021-03-03 11:20:12 +0300

    Добавлена маршрутизация к компонентам. Модуль 2 завершен

--- guess-melody-5/src/components/app/app.jsx
+++ guess-melody-5/src/components/app/app.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
+import AuthScreen from "../auth-screen/auth-screen";
+import GameOverScreen from "../game-over-screen/game-over-screen";
+import WinScreen from "../win-screen/win-screen";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
 
-  return <WelcomeScreen errorsCount={errorsCount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={errorsCount} />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+        <Route exact path="/result">
+          <WinScreen />
+        </Route>
+        <Route exact path="/lose">
+          <GameOverScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
